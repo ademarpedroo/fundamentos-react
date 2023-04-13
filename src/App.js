189a22508e9988e9ad4cfb08ff23977ed9c769de
Ademar Pedro <ademarpedro2012@gmail.com>
2023-04-13 19:30:20 -0300

    primeiro commit fundamentos react

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+import Post from './Post'
+import Header from './Header'
+
+// Props -> Propriedades
+function App() {
+  const [posts, setPosts] = useState([
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 50 },
+  ]);
+
+  function handleRefresh() {
+    setTimeout(() =>{
+      setPosts((prevState) => [
+        ...prevState,
+        {
+          id: Math.random(),
+          title: `Title#${prevState.length + 1}`,
+          subtitle: `Sub#${prevState.length + 1}`,
+          likes: 50
+        },
+      ]);
+    },2000);
+  }
+
+  return (
+    <>
+    <Header>
+      <h2>
+        Posts da semana
+        <button onClick={handleRefresh}>Atualizar</button>
+      </h2>
+    </Header>
+
+    <hr />
+
+    {posts.map(post => (
+      <Post 
+        key={post.id}
+        likes={post.likes}
+        post={{
+          title: post.title,
+          subtitle: post.subtitle
+        }}
+      />
+    ))}
+    </>
+  );
+}
+
+export default App;
