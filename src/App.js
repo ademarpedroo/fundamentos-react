463fd8268ac87aa13e84fe969b345bbcf52b397b
Ademar Pedro <ademarpedro2012@gmail.com>
2023-04-18 19:36:04 -0300

    Adicionando campo de excluir

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Header from './Header'
 // Props -> Propriedades
 function App() {
   const [posts, setPosts] = useState([
-    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
-    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
-    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
-    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 50 },
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 50, read: false },
   ]);
 
   function handleRefresh() {
@@ -18,12 +18,18 @@ function App() {
         ...prevState,
         {
           id: Math.random(),
-          title: `Title#${prevState.length + 1}`,
-          subtitle: `Sub#${prevState.length + 1}`,
+          title: `Title#0${prevState.length + 1}`,
+          subtitle: `Sub#0${prevState.length + 1}`,
           likes: 50
         },
       ]);
     },2000);
+  }
+
+  function handleRemovePost(postId) {
+    setPosts((prevState) => ( 
+      prevState.filter(post => post.id !== postId)
+    ));
   }
 
   return (
@@ -41,10 +47,8 @@ function App() {
       <Post 
         key={post.id}
         likes={post.likes}
-        post={{
-          title: post.title,
-          subtitle: post.subtitle
-        }}
+        onRemove={handleRemovePost}
+        post={post}
       />
     ))}
     </>
