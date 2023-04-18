9d6315d1d8767531bc3b582aaac9ed0d984aabaa
Ademar Pedro <ademarpedro2012@gmail.com>
2023-04-18 20:38:36 -0300

    Reestruturando código para mudar tema

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import React, { useState, createContext } from 'react';
+import React, { useState } from 'react';
 
 import Post from './Post'
 import Header from './Header'
-
-export const ThemeContext = createContext('dark');
-
+import { ThemeProvider } from './ThemeContext';
 
 // Props -> Propriedades
 function App() {
-  const [theme, setTheme] = useState('dark');
-
   const [posts, setPosts] = useState([
     { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false },
     { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true },
     { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false },
     { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 50, read: false },
   ]);
-
-  function handleToggleTheme(){
-    setTheme((prevState) => (
-      prevState == 'dark' 
-        ? 'light' 
-        : 'dark'
-    ));
-  }
 
   function handleRefresh() {
     setTimeout(() =>{
@@ -46,12 +34,7 @@ function App() {
   }
 
   return (
-    <ThemeContext.Provider 
-      value={{ 
-        theme,
-        onToggleTheme: handleToggleTheme,
-      }}
-    >
+    <ThemeProvider>
     <Header >
       <h2>
         Posts da semana
@@ -69,7 +52,7 @@ function App() {
         post={post}
       />
     ))}
-    </ThemeContext.Provider>
+    </ThemeProvider>
   );
 }
 
